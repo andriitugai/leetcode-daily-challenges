b54062583adf525bd9580b4b886cc4cc3e3ebed0
Andrii Tugai <andrii.tugai@gmail.com>
2023-10-12 21:42:40 +0300

    Added solution for 33. Search in Rotated Sorted Array

---
+++ 0033-search-in-rotated-sorted-array/solution.go
@@ -0,0 +1,25 @@
+unc search(nums []int, target int) int {
+    var l, r int = 0, len(nums) - 1
+
+    for l <= r {
+        m := (l + r) / 2
+        if nums[m] == target {
+            return m
+        }
+
+        if nums[l] <= nums[m] {
+            if nums[l] <= target && target < nums[m] {
+                r = m - 1
+            } else {
+                l = m + 1
+            }
+        } else {
+            if nums[m] < target && target <= nums[r] {
+                l = m + 1
+            } else {
+                r = m - 1
+            }
+        }
+    }
+    return -1
+}
