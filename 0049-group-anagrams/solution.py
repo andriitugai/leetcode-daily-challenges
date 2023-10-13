db0f91884ae4c31ce1906816071bd6af145263a7
Andrii Tugai <andrii.tugai@gmail.com>
2023-10-13 09:47:02 +0300

    Added solution for

---
+++ 0049-group-anagrams/solution.py
@@ -0,0 +1,7 @@
+lass Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = collections.defaultdict(list)
+        for word in strs:
+            groups[tuple(sorted(list(word)))].append(word)
+            
+        return groups.values()
