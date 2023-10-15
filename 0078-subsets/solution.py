b8a0715d2b4db2498e02dba4f7f359869fa7f6f0
Andrii Tugai <andrii.tugai@gmail.com>
2023-10-15 19:30:18 +0300

    Added solution for 78. Subsets

---
+++ 0078-subsets/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        for num in nums:
+            result += [subset + [num] for subset in result]
+            
+        return result
