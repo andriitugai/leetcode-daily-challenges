de5ee0e666612605f044f1fd3b6ef6125606cd6d
Andrii Tugai <andrii.tugai@gmail.com>
2023-10-25 10:34:32 +0300

    Added solution for 779. K-th Symbol in Grammar

---
+++ from-0701-to-0800/0779-k-th-symbol-in-grammar/solution.go
@@ -0,0 +1,11 @@
+func kthGrammar(n int, k int) int {
+    if n == 1 {
+        return 0
+    }
+    length := 1 << (n - 2)
+    if k <= length {
+        return kthGrammar(n - 1, k)
+    } else {
+        return 1 - kthGrammar(n - 1, k - length)
+    }
+}
