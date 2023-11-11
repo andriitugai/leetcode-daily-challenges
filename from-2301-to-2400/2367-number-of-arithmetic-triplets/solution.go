0ac346baa65bb4241d01e4ee0652352882b00759
Andrii Tugai <andrii.tugai@gmail.com>
2023-11-11 02:25:25 +0200

    Fixed solution for 2367

--- from-2301-to-2400/2367-number-of-arithmetic-triplets/solution.go
+++ from-2301-to-2400/2367-number-of-arithmetic-triplets/solution.go
@@ -1,13 +1,9 @@
 func arithmeticTriplets(nums []int, diff int) int {
     presence := make(map[int]bool)
-    for _, n := range nums {
-        presence[n] = true
-    }
-
     result := 0
-    diff2 := 2 * diff
     for _, num := range nums {
-        if presence[num + diff] && presence[num + diff2] {
+        presence[num] = true
+        if num >= diff * 2 && presence[num - diff] && presence[num - diff * 2] {
             result += 1
         }
     }
