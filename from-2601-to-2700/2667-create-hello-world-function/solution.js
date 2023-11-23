67e19bcef549d0abe2d5adfe2c7636631b78a14e
Andrii Tugai <andrii.tugai@gmail.com>
2023-11-23 18:39:29 +0200

    Added solution for 2667. Create Hello World Function

---
+++ from-2601-to-2700/2667-create-hello-world-function/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        // return value
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
