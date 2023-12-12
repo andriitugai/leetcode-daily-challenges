9360325419b98da2dcc7843c11fad38e40847ab3
Andrii Tugai <andrii.tugai@gmail.com>
2023-12-12 07:31:19 +0200

    Added solution for 1464. Maximum Product of Two Elements in an Array

---
+++ from-1401-to-1500/1464-maximum-product-of-two-elements-in-an-array/solution.go
@@ -0,0 +1,12 @@
+func maxProduct(nums []int) int {
+    m1, m2 := 0, 0
+    for _, num := range nums {
+        if num > m1 {
+            m2 = m1
+            m1 = num
+        } else if num > m2 {
+            m2 = num
+        }
+    }
+    return (m1 - 1) * (m2 - 1)
+}
