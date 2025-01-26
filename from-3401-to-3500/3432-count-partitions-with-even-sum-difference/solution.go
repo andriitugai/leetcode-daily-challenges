d8e6e5bdb476c9c46a1c30e26f317f7144eda5f8
Andrii Tugai <andrii.tugai@gmail.com>
2025-01-26 16:25:25 +0200

    Added solution for 3432. Count Partitions with Even Sum Difference

--- from-3401-to-3500/3432-count-partitions-with-even-sum-difference/solution.go
+++ from-3401-to-3500/3432-count-partitions-with-even-sum-difference/solution.go
@@ -1,5 +1,4 @@
 func countPartitions(nums []int) int {
-    n := len(nums)
     total := 0
     for _, num := range(nums) {
         total += num
@@ -8,14 +7,5 @@ func countPartitions(nums []int) int {
         return 0
     }
 
-    curr := 0
-    result := 0
-    for i := 0; i < n - 1; i ++ {
-        curr += nums[i]
-        total -= nums[i]
-        if total % 2 == curr % 2 {
-            result += 1
-        }
-    }
-    return result
+    return len(nums) - 1
 }
--- from-3401-to-3500/3432-count-partitions-with-even-sum-difference/solution.py
+++ from-3401-to-3500/3432-count-partitions-with-even-sum-difference/solution.py
@@ -4,12 +4,4 @@ class Solution:
         if total % 2:
             return 0
 
-        curr = 0
-        result = 0
-        for num in nums[:-1]:
-            curr += num
-            total -= num
-            if curr % 2 == total % 2:
-                result += 1
-
-        return result
+        return len(nums) - 1
