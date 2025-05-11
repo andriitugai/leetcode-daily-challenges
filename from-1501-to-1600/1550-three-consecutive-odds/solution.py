b1df3ac4f69d6475664438fe013ff5b50fa19376
Andrii Tugai <andrii.tugai@gmail.com>
2025-05-11 07:23:20 +0300

    Added solution for 1550. Three Consecutive Odds

--- from-1501-to-1600/1550-three-consecutive-odds/solution.py
+++ from-1501-to-1600/1550-three-consecutive-odds/solution.py
@@ -1,19 +1,12 @@
-func threeConsecutiveOdds(arr []int) bool {
-    i := 0
-    for i < len(arr)-2 {
-        if arr[i] % 2 == 1 {
-            if arr[i+1] % 2 == 1 {
-                if arr[i+2] % 2 == 1 {
-                    return true
-                } else {
-                    i += 3
-                }
-            } else {
-                i += 2
-            }
-        } else {
-            i += 1
-        }
-    }
-    return false
-}
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        odds = 0
+        for num in arr:
+            if num % 2:
+                odds += 1
+                if odds == 3:
+                    return True
+            else:
+                odds = 0
+
+        return False
